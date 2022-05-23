b8d8e0e62d0329980d9bde83383e6ac0bb42981f
leozhou <492753662@qq.com>
2022-05-23 20:27:33 +0800

    Initial Commit

---
+++ Shared/RealmToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmToDoDemoApp.swift
+//  Shared
+//
+//  Created by 周椿杰 on 2022/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RealmToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
